b8009224345da434d01cb339c893a6e9dc8fb162
Berryluxo <lina.avramenko27@gmail.com>
2017-01-28 12:16:59 +0200

    first commit

---
+++ src/Module1/SecondClass.java
@@ -0,0 +1,10 @@
+package Module1;
+
+/**
+ * Created by Lina.Avramenko on 28.01.2017.
+ */
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("Мой первый класс на Java");
+    }
+}
