789d0c90bb99c6e19d8d6077f019dc34b2f50562
Berryluxo <lina.avramenko27@gmail.com>
2017-01-30 21:54:58 +0200

    fourth commit. Task2

--- src/Module2/Task2.java
+++ src/Module2/Task2.java
@@ -4,4 +4,19 @@ package Module2;
  * Created by Lina.Avramenko on 30.01.2017.
  */
 public class Task2 {
+    public static void main(String[] args) {
+        int balance, withdrawal;
+        result(200, 199);
+    }
+
+    static void result(int balance, int withdrawal) {
+        int commision = 5;
+        double comTrans = (double) withdrawal * (double) commision / 100;
+        double result = balance - withdrawal - comTrans;
+        if (result >= 0)
+            System.out.println("OK " + comTrans + " " + result);
+        else
+            System.out.println("NO");
+
+    }
 }
