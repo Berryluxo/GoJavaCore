24c42eccb816a2755c5095ee16397b02aaf595dc
Lina <lina.avramenko27@gmail.com>
2017-03-20 16:45:42 +0200

    For tasks 5.8-5.10

--- src/Module5/DAOImpl.java
+++ src/Module5/DAOImpl.java
@@ -1,27 +1,68 @@
 package Module5;
 
 /**
- * Created by Lina.Burkalo on 11.03.2017.
+ * Created by Vadym on 20.03.2017.
  */
 public class DAOImpl implements DAO {
+    Room rooms[] = new Room[10];
 
+    @Override
     public Room save(Room room) {
-        System.out.println("Save room: " + room.getId());
+        for (int i = 0; i < rooms.length; i++) {
+            if (rooms[i] == null)
+                rooms[i] = room;
+            break;
+
+
+        }
+
+        System.out.println("save " + room);
         return room;
     }
 
+    @Override
     public boolean delete(Room room) {
-        System.out.println("Delete room: " + room.getId());
+        for (int i = 0; i < rooms.length; i++) {
+            if (rooms[i].equals(room)) {
+                rooms[i] = null;
+                break;
+            }
+
+        }
+
+        System.out.println("delete " + room);
         return true;
     }
 
+    @Override
     public Room update(Room room) {
-        System.out.println("Save room: " + room.getId());
+        for (int i = 0; i < rooms.length; i++) {
+            if (rooms[i].getId() == room.getId()) {
+                rooms[i] = room;
+                break;
+            }
+
+        }
+        System.out.println("update " + room);
         return room;
     }
 
+    @Override
     public Room findById(long id) {
-        System.out.println("Find by id room: " + id);
+        for (int i = 0; i < rooms.length; i++) {
+            if (rooms[i].getId() == id) {
+                return rooms[i];
+
+            }
+
+        }
+
+        System.out.println("find " + id);
         return null;
+    }
+
+    @Override
+    public Room[] getAll() {
+        return new Room[0];
     }
 }
